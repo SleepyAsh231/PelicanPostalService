76f6d4b9a47e4b153b8ea3aadabca9376d1fbceb
SleepyAsh231 <74946574+SleepyAsh231@users.noreply.github.com>
2020-11-23 23:14:15 -0500

    Update ModConfig.cs

--- PelicanPostalService/Config/ModConfig.cs
+++ PelicanPostalService/Config/ModConfig.cs
@@ -3,7 +3,7 @@
     public class ModConfig
     {
         public string MenuAccessKey { get; set; } = "Y";
-        public bool AllowQuestSubmissions { get; set; } = false;
-        public bool LazyItemChecking { get; set; } = false;
+        public bool AllowQuestSubmissions { get; set; } = true
+        public bool LazyItemChecking { get; set; } = true
     }
 }
